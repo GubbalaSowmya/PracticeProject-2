d1077a89af0940f20159ce84d0fca8a6b91208ca
Sowmya G <sowmyagubbala0681@gmail.com>
2022-09-15 12:45:06 +0530

    Add project files.

---
+++ PracticeProject-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"D:\C#\PracticeProject-2\PracticeProject-2\TextFile.txt";
+            Console.WriteLine("Reading File using File.ReadAllText()");
+            if (File.Exists(file))
+            {
+                {
+                    string str = File.ReadAllText(file);
+                    Console.WriteLine(str);
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+    }
+}
